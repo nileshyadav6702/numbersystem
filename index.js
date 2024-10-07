6273596f4ad8637eb0301a52fed1212fa4b8ab32
Nilesh yadav <rameshkumaryadav1918@gmail.com>
2024-10-07 12:45:41 +0530

    add indes.jswill finds odd and even numbers from an array

---
+++ index.js
@@ -0,0 +1,13 @@
+const numbers = [
+    3, 7, 2, 15, 9, 20, 5, 14, 12, 1, 8, 11, 6, 19, 4, 10, 17, 13, 16, 18,
+  ];
+
+for(let i=0;i<numbers.length;i++){
+    if(i%2==0){
+        console.log(i,'is a even number')
+    }
+    else{
+        console.log(i,'is a odd number')
+
+    }
+}
